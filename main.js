e84d0ee742f1c9ce1dfcfecc91cd9cdd975798cc
Rajaranjith08 <rajaranjith842001@gmail.com>
2024-06-13 17:29:43 +0530

    Route Struture

--- main.js
+++ main.js
@@ -1,10 +1,33 @@
-const express = require("express")
+const express = require("express");
 
-const app = express()
+const app = express();
 
 
 app.get('/',(req,res)=>{
     res.json({msg:'hello student'})
+});
+
+// CURD functionalistyof movies
+
+// R - for reading
+app.get('/movies',(req,res)=>{
+
+});
+
+// C - for creating
+app.post('/movies',(req,res)=>{
+
+});
+
+// U - for updating movie
+app.put('/movies/:id',(req,res)=>{
+
+});
+
+// D - for deleting movie
+app.delete('/movies:id',(req,res)=>{
+
 })
 
-app.listen(3000)
+
+app.listen(3000);
