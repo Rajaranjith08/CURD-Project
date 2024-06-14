b5e2bb8e8c9140accc465d1c8627fa93caceec67
Rajaranjith08 <rajaranjith842001@gmail.com>
2024-06-14 13:47:16 +0530

    connecting the mongoose and mongodb server

--- README.md
+++ README.md
@@ -12,3 +12,22 @@ provide a movie api for react.js app.
 4. `git add .`
 5. `git commit -m "your massage"`
 6. `git push -u origin main or git push`
+7. `npm install mongodb`
+8. `npm install mongoose`
+
+
+### database noSQL
+
+1. mongodb
+2. mongoose
+
+### connecting the databse using mongoose 
+how to coonect?
+step-1: create const eg:
+            const dbURI= ( get url from mongodb server and paste it (note: dont use the space insid a url link) )
+step-2: connect a mongodb and mongoose
+            const mongoose = require(" mongoose ")
+step-3: create a promise for this fucntion
+            mongoose.connect(dbURI)
+                .then( result => app.listen(3000) )
+                .catch ( err => console.log(err) )
--- main.js
+++ main.js
@@ -1,9 +1,17 @@
 const express = require("express");
-const movieRoute = require("./routes/movies.route")
+const mongoose = require("mongoose")
+const movieRoute = require("./routes/movies.route");
 
-
-
+// express app
 const app = express();
+
+//connecting to mongodb 
+const dbURI =  `mongodb+srv://rajaranjithmovieProject:1234567890@firstprojectnodejs.yy4bi8x.mongodb.net/movieAPI-project?retryWrites=true&w=majority&appName=firstProjectNodeJS`
+mongoose.connect(dbURI)
+    .then( result => {
+        app.listen(3000);
+        console.log("listening for request from 3000")} )
+    .catch(err => console.log(err.message))
 
 
 app.get ('/',(req,res)=>{
@@ -18,4 +26,3 @@ app.use("/movies", movieRoute)
 
 
 
-app.listen(3000);
